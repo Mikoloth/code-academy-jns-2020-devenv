8836bbf932af582e7a0cab26c0e14d5adcd79b1b
mikoloth <mikoloth@gmail.com>
2020-07-03 11:06:07 +0300

    Fruitseille hajotus controlleriin ja repositoryyn

--- userservice/main.js
+++ userservice/main.js
@@ -1,121 +1,84 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
 
+const {
+  readFruits,
+  readRedFruits,
+  createFruit,
+} = require("./fruits/FruitController");
+
 const app = express();
 const port = 3000;
 const mongoDbUrl = "mongodb://root:example@mongo:27017/";
 
 app.use(express.json());
 
-const fakeUsers = [
-    {
-        id: 1,
-        name: "Admin",
-        email: "admin@example.com",
-        password: "admin",
-    },
-    {
-        name: "Ville",
-        email: "ville@example.com",
-        password: "ville",
-        id: 2,
-    },
-];
-
 app.get("/hello", (req, res) => res.send("Hello World!!!"));
 
 const bootTime = new Date();
 app.get("/heartbeat", (req, res) => {
-    const now = new Date();
-    res.send(`
+  const now = new Date();
+  res.send(`
   <div>
     <div>Boot time: ${bootTime.toLocaleString("fi-FI", {
-        timeZone: "Europe/Helsinki",
+      timeZone: "Europe/Helsinki",
     })}</div>
     <div>Current time: ${now.toLocaleString("fi-FI", {
-        timeZone: "Europe/Helsinki",
+      timeZone: "Europe/Helsinki",
     })}</div>
     <div>Uptime: ${now - bootTime}ms</div>
   </div>
   `);
 });
 
-app.get("/fruits", (req, res) => {
-    MongoClient.connect(mongoDbUrl, (err, dbconn) => {
-        if (err) throw err;
-        const dbo = dbconn.db("test");
-        const collection = dbo.collection("fruits");
-        collection.find({}).toArray((err, result) => {
-            if (err) throw err;
-            res.json(result);
-            dbconn.close();
-        });
-    });
-});
-
-app.post("/fruits", (req, res) => {
-    const newFruit = req.body;
-    MongoClient.connect(mongoDbUrl, (err, dbconn) => {
-        if (err) throw err;
-        const dbo = dbconn.db("test");
-        const collection = dbo.collection("fruits");
-        collection.insertOne(newFruit, (err, dbRes) => {
-            if (err) throw err;
-            res.json(dbRes.ops[0]);
-            dbconn.close();
-        });
-    });
-});
+app.post("/fruits", createFruit);
+app.get("/fruits", readFruits);
+app.get("/redFruits", readRedFruits);
 
 app.get("/users", (req, res) => {
-    MongoClient.connect(mongoDbUrl, (err, dbconn) => {
-        if (err) throw err;
-        const dbo = dbconn.db("userservice");
-        const collection = dbo.collection("users");
-        collection.find({}).toArray((err, result) => {
-            if (err) throw err;
-            res.json(result);
-            dbconn.close();
-        });
+  MongoClient.connect(mongoDbUrl, (err, dbconn) => {
+    if (err) throw err;
+    const dbo = dbconn.db("userservice");
+    const collection = dbo.collection("users");
+    collection.find({}).toArray((err, result) => {
+      if (err) throw err;
+      res.json(result);
+      dbconn.close();
     });
-});
-
-/*app.delete("/users/:userId", (req, res) => {
-    const userId = parseInt(req.params.userId);
-    const index = fakeUsers.findIndex((user) => user.id === userId);
-    if (index >= 0) {
-        fakeUsers.splice(index, 1);
-    }
-    res.end();
-});*/
-
-app.post("/users", (req, res) => {
-    const newUser = req.body;
-    MongoClient.connect(mongoDbUrl, (err, dbconn) => {
-        if (err) throw err;
-        const dbo = dbconn.db("userservice");
-        const collection = dbo.collection("users");
-        collection.insertOne(newUser, (err, dbRes) => {
-            if (err) throw err;
-            res.json(dbRes.ops[0]);
-            dbconn.close();
-        });
-    });
+  });
 });
 
 app.delete("/users/:userId", (req, res) => {
-    MongoClient.connect(mongoDbUrl, (err, dbconn) => {
-        if (err) throw err;
-        const dbo = dbconn.db("userservice");
-        const collection = dbo.collection("users");
-        collection.deleteOne({ _id: ObjectId(req.params.userId) }, (err, dbRes) => {
-            if (err) throw err;
-            res.end();
-            dbconn.close();
-        });
+  const userId = req.params.userId;
+  MongoClient.connect(mongoDbUrl, (err, dbconn) => {
+    if (err) throw err;
+    const dbo = dbconn.db("userservice");
+    const collection = dbo.collection("users");
+
+    const deleteQuery = { _id: ObjectId(userId) };
+    console.log({ deleteQuery });
+    collection.deleteOne(deleteQuery, (err, dbRes) => {
+      if (err) throw err;
+      console.log({ dbRes });
+      res.end();
+      dbconn.close();
     });
+  });
+});
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+  MongoClient.connect(mongoDbUrl, (err, dbconn) => {
+    if (err) throw err;
+    const dbo = dbconn.db("userservice");
+    const collection = dbo.collection("users");
+    collection.insertOne(newUser, (err, dbRes) => {
+      if (err) throw err;
+      res.json(dbRes.ops[0]);
+      dbconn.close();
+    });
+  });
 });
 
 app.listen(port, () =>
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 );
